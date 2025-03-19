19e760597a8f51733f9615eaa68cb525ec2cbd83
OmriOmri31 <omriomrinir@gmail.com>
2025-03-19 19:31:23 +0200

    LogIn UI

--- ja-moveo/src/components/LogIn.js
+++ ja-moveo/src/components/LogIn.js
@@ -1,10 +1,10 @@
+// src/components/LogIn.js
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const LogIn = () => {
     const [nickname, setNickname] = useState("");
     const [password, setPassword] = useState("");
-
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
@@ -16,12 +16,12 @@ const LogIn = () => {
         });
         const data = await response.json();
         if (response.ok) {
-            localStorage.setItem('token', data.token);
-            localStorage.setItem('nickname', nickname);
-            localStorage.setItem('image', data.image);
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("nickname", nickname);
+            localStorage.setItem("image", data.image);
             localStorage.setItem("loggedIn", "true");
-            localStorage.setItem('instrument', data.instrument);
-            localStorage.setItem('isAdmin', data.isAdmin.toString());
+            localStorage.setItem("instrument", data.instrument);
+            localStorage.setItem("isAdmin", data.isAdmin.toString());
             data.isAdmin ? navigate("/HomeAdmin") : navigate("/Home");
         } else {
             alert("Try Again!");
@@ -29,23 +29,26 @@ const LogIn = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="Nickname"
-                value={nickname}
-                onChange={(e) => setNickname(e.target.value)}
-                required
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-            />
-            <button type="submit">Log In</button>
-        </form>
+        <div className="page-container">
+            <h2 className="page-title">Log In</h2>
+            <form className="page-form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nickname"
+                    value={nickname}
+                    onChange={(e) => setNickname(e.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button type="submit">Log In</button>
+            </form>
+        </div>
     );
 };
 
